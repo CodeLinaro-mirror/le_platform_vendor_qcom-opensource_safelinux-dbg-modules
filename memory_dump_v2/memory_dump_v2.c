4fe7119249ef33a93f0a32dfe50e86d41493cfe2
Sankalp Negi <snegi@qti.qualcomm.com>
2025-11-16 23:50:13 -0800

    memory_dump_v2: Switch to TZMEM Interfaces
    QTEE SHMBRIDGE interfaces are now deprecated, use TZMEM
    interfaces to align with the upstream infrastructure.
    
    Change-Id: Ia5c1b7c566f2abafbf5309992e6d96b207fbea86
    Signed-off-by: Sankalp Negi <snegi@qti.qualcomm.com>
    (cherry picked from commit 135a970f41b86b0e36aa2177928f3ab394e7dbcc)

--- memory_dump_v2/memory_dump_v2.c
+++ memory_dump_v2/memory_dump_v2.c
@@ -13,7 +13,7 @@
 #include <linux/of_address.h>
 #include <minidump.h>
 #include <memory_dump.h>
-#include <linux/qtee_shmbridge.h>
+#include <linux/firmware/qcom/qcom_tzmem.h>
 #include <soc/qcom/secure_buffer.h>
 #include <linux/of_device.h>
 #include <linux/dma-mapping.h>
@@ -1101,7 +1101,7 @@ static int mem_dump_alloc(struct platform_device *pdev)
 	sg_free_table(&mem_dump_sgt);
 
 	memset(dump_vaddr, 0x0, total_size);
-	ret = qtee_shmbridge_register(phys_addr, total_size, ns_vmids,
+	ret = qcom_tzmem_register(phys_addr, total_size, ns_vmids,
 			ns_vm_perms, 1, PERM_READ|PERM_WRITE, &shm_bridge_handle);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to create shm bridge.ret=%d\n", ret);
@@ -1111,7 +1111,7 @@ static int mem_dump_alloc(struct platform_device *pdev)
 	ret = init_memory_dump(dump_vaddr, phys_addr);
 	if (ret) {
 		dev_err(&pdev->dev, "Memory Dump table set up is failed\n");
-		qtee_shmbridge_deregister(shm_bridge_handle);
+		qcom_tzmem_deregister(shm_bridge_handle);
 		return ret;
 	}
 
@@ -1119,7 +1119,7 @@ static int mem_dump_alloc(struct platform_device *pdev)
 	if (ret) {
 		ret = init_memdump_imem_area(total_size);
 		if (ret) {
-			qtee_shmbridge_deregister(shm_bridge_handle);
+			qcom_tzmem_deregister(shm_bridge_handle);
 			return ret;
 		}
 	}
